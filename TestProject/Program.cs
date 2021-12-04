9e6f0315719d90eb94c25083f6765e277b935cf9
Valeriy-01 <73665951+Valeriy-01@users.noreply.github.com>
2021-12-04 11:21:15 +0300

    Add files via upload

---
+++ TestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            await new Pipeline().Generate(2,
+                @"C:\OneDrive\Maxim\OneDrive\Desktop\Lab4\GeneratedTests",
+                @"C:\OneDrive\Maxim\OneDrive\Desktop\Lab4\TestProject\TestClass.cs",
+                @"C:\OneDrive\Maxim\OneDrive\Desktop\Lab4\TestProject\AnotherTestClass.cs"); 
+        }
+    }
+}
